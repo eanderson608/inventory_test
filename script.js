bd596cbbeca846653b3c5e1e6785aa1d9634ea61
Eric Anderson <eanderson@macbook-eric.local>
2018-04-15 10:15:32 -0500

    unzip icons, update action button css

--- script.js
+++ script.js
@@ -25,6 +25,8 @@ function addItem(type, count) {
         if (cells[i].firstChild == null && cells[i].parentNode.style.visibility != 'hidden') {
 
             var item = document.createElement('div');
+            var text = document.createElement('span');
+            item.appendChild(text);
             item.setAttribute('data-item-type', type);
             item.setAttribute('data-item-count', count);
             item.classList.add('inventory-item');
@@ -34,9 +36,9 @@ function addItem(type, count) {
             var now = new Date();
             item.id = now.getTime();
 
-            item.style.backgroundImage = "url('coffee.png')";
+            item.style.backgroundImage = "url('WEED.png')";
 
-            item.innerHTML = count;
+            text.innerHTML = count;
 
             cells[i].appendChild(item);
             break;
@@ -58,13 +60,15 @@ function drop(ev) {
     var source = document.getElementById(data)
     if (ev.target.firstChild == null) {
         ev.target.appendChild(source);
+
     } else if (source.getAttribute('data-item-type') == ev.target.getAttribute('data-item-type') && source.id != ev.target.id) {
         c1 = Number(source.getAttribute('data-item-count'));
         c2 = Number(ev.target.getAttribute('data-item-count'));
         newCount = (c1 + c2).toFixed(1)
         source.parentNode.removeChild(source);
         ev.target.setAttribute('data-item-count', newCount);
-        ev.target.innerHTML = newCount;
+        ev.target.getElementsByTagName('span')[0].innerHTML = newCount;
+
     } else if (source.getAttribute('data-item-type') != ev.target.getAttribute('data-item-type') && ev.target.getAttribute('data-item-type') != null) {
         var parentTarget = ev.target.parentNode;
         var parentSource = source.parentNode;
